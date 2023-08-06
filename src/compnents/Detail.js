93fb271f3389d462e13a025bd672aae9322801c6
= <=>
2023-08-06 10:06:13 +0530

    update

--- src/compnents/Detail.js
+++ src/compnents/Detail.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
 import '../style/Detail.css';
+import buttonLogo from '../images/Group 2934.png';
+import pathLogo from '../images/Path.png';
+
+
 
 const ItemDetail = () => {
   const { id } = useParams();
@@ -21,13 +24,24 @@ const ItemDetail = () => {
         <h1 className="logo">TravelMedia.in</h1>
         <div className="toggle-menu">
           <span className="material-icons">home</span>
-          <span className="material-icons">search</span>
+          <span className="material-icons">notifications</span>
+          <img className='pathLogo' src={pathLogo} alt="pathLogo" />
           <span className="material-icons">person</span>
         </div>
       </nav>
-      <div>
+      <div className='section-container'>
         <div className='display-container'>
-          <img style={{width: 300}} src={item.imgSrc} alt={`Item ${item.id}`} />
+          <div className='image-container'>
+            <img src={item.imgSrc} alt={`Item ${item.id}`} />
+            <div className='title-container'>
+              <h3>{item.title.slice(0,20)}{item.title.length > 20 ? '...' : ''}</h3>
+              <div>
+                <span className="material-icons">share</span>
+                <span className="material-icons">favorite</span>
+              </div>
+            </div>
+          </div>
+          
           <div className='info-container'>
             <div className='button-container'>
               <button onClick={()=> setShowDetails(true)} className="details-btn">details</button>
@@ -49,20 +63,27 @@ const ItemDetail = () => {
       <div className='more-posts-container'>
         <h1>More Posts</h1>
         <ul className="item-list">
-            {items.map(item => (
-                <li key={item.id} className="item-card">
-                    <Link to={`/item/${item.id}`}>
-                    <img src={item.imgSrc} alt={`Item ${item.id}`} />
-                    <h2 className="item-title">{item.title}</h2>
-                    <p className="item-description">
-                        {item.body.slice(0, 100)}{item.body.length > 100 ? '...' : ''}
-                    <a className="read-more">Read More...</a>
-                    </p>
+        {items.map(item => (
+          <li key={item.id} className="item-card">
+            <Link to={`/item/${item.id}`}>
+            <img className='postImg' src={item.imgSrc} alt={`Item ${item.id}`} />
+                    <div className="item-info">
+                      <div>
+                          <h2 className="item-title">{item.title}</h2>
+                          <p className="item-description">
+                              {item.body.slice(0, 100)}{item.body.length > 100 ? '...' : ''}
+                              <a className="read-more">Read More...</a>
+                          </p>
+                      </div>
+                      <div>
+                        <img src={buttonLogo} alt="buttonLogo" />
+                      </div>
+                  </div>
               
-                    </Link>
-                </li>
-            ))}
-        </ul>
+            </Link>
+          </li>
+        ))}
+      </ul>
       </div>
     </div>
     </div>
--- src/compnents/Home.js
+++ src/compnents/Home.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchItems } from '../redux/action';
 import { Link } from 'react-router-dom';
 import '../style/Home.css';
+import buttonLogo from '../images/Group 2934.png';
+import pathLogo from '../images/Path.png';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -18,29 +20,39 @@ const Home = () => {
         <h1 className="logo">TravelMedia.in</h1>
         <div className="toggle-menu">
           <span className="material-icons">home</span>
-          <span className="material-icons">search</span>
+          <span className="material-icons">notifications</span>
+          <img className='pathLogo' src={pathLogo} alt="pathLogo" />
           <span className="material-icons">person</span>
         </div>
       </nav>
-      <h1 className="home-title">Social Media For Travellers</h1>
-      <div className='search-container'>
-        <input type='text' placeholder='Search' />
-        </div>
-      <ul className="item-list">
+      <div className='section-container'>
+          <h1 className="home-title">Social Media For Travellers</h1>
+          <div className='search-container'>
+            <input type='text' placeholder='Search' />
+          </div>
+          <ul className="item-list">
         {items.map(item => (
           <li key={item.id} className="item-card">
             <Link to={`/item/${item.id}`}>
-            <img src={item.imgSrc} alt={`Item ${item.id}`} />
-              <h2 className="item-title">{item.title}</h2>
-              <p className="item-description">
-                {item.body.slice(0, 100)}{item.body.length > 100 ? '...' : ''}
-                <a className="read-more">Read More...</a>
-              </p>
+            <img className='postImg' src={item.imgSrc} alt={`Item ${item.id}`} />
+                    <div className="item-info">
+                      <div>
+                          <h2 className="item-title">{item.title}</h2>
+                          <p className="item-description">
+                              {item.body.slice(0, 100)}{item.body.length > 100 ? '...' : ''}
+                              <a className="read-more">Read More...</a>
+                          </p>
+                      </div>
+                      <div>
+                        <img src={buttonLogo} alt="buttonLogo" />
+                      </div>
+                  </div>
               
             </Link>
           </li>
         ))}
       </ul>
+      </div>
     </div>
   );
 };
